cee98cdbdc293e3f2afa00282e5c2adcce83dc71
Jose Pedro Oliveira <jose.p.oliveira.oss@gmail.com>
2018-10-28 16:56:40 +0100

    drv_mellanox.cc: typo

--- drv_mellanox.cc
+++ drv_mellanox.cc
@@ -11,7 +11,7 @@
 
 #include "parser.h"
 
-static RegexParser mellaonx_mlx5_core( 
+static RegexParser mellanox_mlx5_core( 
 	{ "mlx5_core" },
 	{ "^(rx|tx)_(bytes|packets)$", { 1, 2} },
 	{ "^(rx|tx)(\\d+)_(?:0_)?(bytes|packets)$", { 1, 3, 2 } }
