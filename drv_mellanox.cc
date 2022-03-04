5ce70436b1c344f8f1fd4d1a7abab8581c344293
Ray Bellis <ray@isc.org>
2022-03-04 15:37:14 +0000

    add additional Mellanox driver identifier

--- drv_mellanox.cc
+++ drv_mellanox.cc
@@ -12,7 +12,7 @@
 #include "parser.h"
 
 static RegexParser mellanox_mlx5_core( 
-	{ "mlx5_core" },
+	{ "mlx5_core", "mlx4_en" },
 	{ "^(rx|tx)_(bytes|packets)$", { 1, 2} },
 	{ "^(rx|tx)(\\d+)_(?:0_)?(bytes|packets)$", { 1, 3, 2 } }
 );
